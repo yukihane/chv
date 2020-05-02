756d73196a87f4c0ce63a201cd7def8f94712540
yukihane <yukihane.feather@gmail.com>
2020-05-02 17:45:01 +0900

    一旦コミット

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,1 +1,36 @@
-document.body.style.border = "5px solid red";
+// 多くの場合最新50件のみを取得するリンクになっているので
+// URLの /l50 を除去して全件表示する
+const urlPattern = /\/l50$|\/L#sugar$/;
+const href = window.location.href;
+if (urlPattern.test(href)) {
+  window.location.href = href.replace(urlPattern, "/");
+}
+
+const resPattern = /^\.\.\/test\/read\.cgi\/.+?\/.+?\/\d+$/;
+const linkTags = document.getElementsByTagName("a");
+for (const a of linkTags) {
+  const link = a.getAttribute("href");
+  if (link && resPattern.test(link)) {
+    a.onclick = (e) => {
+      e.preventDefault();
+      insertRes(a, a.href);
+    };
+  }
+}
+
+const insertRes = (refNode: HTMLElement, link: string) => {
+  fetch(link)
+    .then((response) => response.text())
+    .then((text) => {
+      const parser = new DOMParser();
+      const htmlDocument = parser.parseFromString(text, "text/html");
+      const content = htmlDocument.getElementsByClassName("thread").item(0);
+      if (content) {
+        insertAfter(content, refNode);
+      }
+    });
+};
+
+const insertAfter = (newNode: Element, referenceNode: HTMLElement) => {
+  referenceNode.parentNode?.insertBefore(newNode, referenceNode.nextSibling);
+};
